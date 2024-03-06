501b9211b474459ff84db60a7e18bdb868b2176d
fawzan3 <fawzansari3@gmail.com>
2024-03-06 11:12:52 +0530

    role migration updated

--- database/migrations/2024_03_06_054056_create_roles_table.php
+++ database/migrations/2024_03_06_054056_create_roles_table.php
@@ -15,6 +15,8 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid');
+            $table->string('role_name');
             $table->timestamps();
         });
     }
